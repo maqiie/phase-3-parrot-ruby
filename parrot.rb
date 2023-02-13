687078d950972fb5b3ca32b2154fd4cf3caa9f27
maqiie <99504010+maqiie@users.noreply.github.com>
2023-02-13 14:15:42 +0300

    completed the lab

--- parrot.rb
+++ parrot.rb
@@ -1,2 +1,12 @@
 # Create method `parrot` that outputs a given phrase and
 # returns the phrase
+# def parrot(string ="squawk!")
+#     puts  string
+#     return string
+
+# end
+def parrot(phrase = "Squawk!")
+    puts phrase
+    return phrase
+  end
+  
